62ba073f068ce3fbc989f5597a86727a58b91d33
lwaziyam <rev.masinalwazi@gmail.com>
2022-09-15 09:11:53 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -18,28 +18,3 @@ void print_to_98(int n);
 void print_times_table(int n);
 
 #endif /* MAIN_H */
-
-TASK 0
-vi 0-putchar.c
-#include "main.h"
-#include <string.h>
-
-/**
- * main - Entry point
- *
- * Description: prints _putchar using putchar prototype
- *
- * Return: Always 0 (Success)
-*/
-
-int main(void)
-{
-	char str[] = "_putchar";
-	int ch;
-
-	for (ch = 0; ch < 8; ++ch)
-		_putchar(str[ch]);
-	_putchar('\n');
-
-	return (0);
-}
